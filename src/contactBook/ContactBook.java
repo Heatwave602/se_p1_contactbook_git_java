4ebb7e0bc21c15a68bbd518ebe7a56b71318f11e
Salvador <sj.martins@fct.unl.pt>
2026-09-19 15:10:07 +0100

    Added GN (getNumber) function in ContactBook.java and the auxiliary method searchNumber.

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -60,6 +60,25 @@ public class ContactBook {
         contacts[searchIndex(name)].setEmail(email);
     }
 
+
+    public String getNumber(int number){
+        return contacts[searchNumber(number)].getName();
+    }
+
+    private int searchNumber(int number) {
+        int i = 0;
+        int result = -1;
+        boolean found = false;
+        while (i<counter && !found)
+            if (contacts[i].getPhone() == number )
+                found = true;
+            else
+                i++;
+        if (found) result = i;
+        return result;
+    }
+
+
     private int searchIndex(String name) {
         int i = 0;
         int result = -1;
@@ -92,5 +111,4 @@ public class ContactBook {
     public Contact next() {
         return contacts[currentContact++];
     }
-
 }
